2fb5870423882db53c51290aa88fb6b4101c0846
uliw <uli.wortmann@utoronto.ca>
2024-01-30 15:21:59 -0500

    revised assignment

--- po4-model/po4.py
+++ po4-model/po4.py
@@ -1,3 +1,19 @@
+def check_convergence(C, c):
+    """check if first derivative is smaller than c
+    C: Array with numbers
+    c: convergence criterion
+    """
+    import numpy as np
+
+    lc = len(C)
+    lc2 = int(lc / 2)
+    diff = np.diff(C)
+    for i, e in enumerate(diff):
+        if e < lc and i > lc2:
+            break
+    return i
+
+
 def dCdt1(t, C_0, V, F_r):
     """Calculate the change in concentration as function of time
     No export productivity
@@ -40,8 +56,8 @@ def dCdt2(t, C_0, V, F_r, THC, tau, r):
                concentratio, i.e., C_s* V / tau wher tau is a scaling factor
     """
 
-    C_s = C_0[0]  # surface ocean concentration
-    C_d = C_0[1]  # deep ocean concentration
+    C_s = C_0[0] / 1000  # surface ocean concentration
+    C_d = C_0[1] / 1000  # deep ocean concentration
     V_s = V[0]  # surface ocean volume
     V_d = V[1]  # deep ocean volume
 
@@ -58,33 +74,43 @@ def dCdt2(t, C_0, V, F_r, THC, tau, r):
     return [dCdt_s, dCdt_d]
 
 
-def po41(time, C_0, V, F_r, dpi, fn):
+def po41(time, C_0, V, F_r, dpi, fn, plot=True):
     """Calculate P concentration over time. Iput flux is
     fixed, output flux depends on concentration
     """
     from scipy.integrate import solve_ivp
     import matplotlib.pyplot as plt
+    import numpy as np
 
     year_to_seconds = 60 * 60 * 24 * 365.2425  # yr to s
     t_span = (0, time * year_to_seconds)  #
     p = (V, F_r)  # function arguments
+    C_0 = np.array(C_0) / 1000  # convert to mol/m^3
 
     # integrate dC/dt
     result = solve_ivp(dCdt1, t_span, C_0, args=p, max_step=t_span[1] / 50)
     t = result.t / year_to_seconds / 1e6  # time in Myr
-    C = result.y.T  # concentration in mol/m^3
-    print(f"Final PO4 concentration {C[-1]} [mol/m^3]")
+    C = result.y.T * 1e3  # concentration in umol/l
+    print(f"Final PO4 concentration {C[-1][0]:.2f} [umol/l]")
+    i = check_convergence(C[:, 0], 1e-3)
+    if i >= len(C[:, 0]):
+        print("Failed to reach steady state. Double the run time")
+    else:
+        print(f"Steady state reached after {t[i]:.1e} Myrs")
 
-    # plot data
-    fig, ax = plt.subplots()
-    ax.plot(t, C)
-    ax.set_xlabel("Time [Myr]")
-    ax.set_ylabel("P concentration [mol/m^3]")
-    fig.tight_layout()
-    ax.grid()
-    plt.show()
-    fig.set_dpi(dpi)
-    fig.savefig(fn)
+    if plot:
+        # plot data
+        fig, ax = plt.subplots()
+        ax.plot(t, C)
+        ax.set_xlabel("Time [Myr]")
+        ax.set_ylabel(r"P concentration [$\mu$mol/l]")
+        fig.tight_layout()
+        ax.grid()
+        plt.show()
+        fig.set_dpi(dpi)
+        fig.savefig(fn)
+
+    return C[:, 0]
 
 
 def po42(time, C_0, V, F_r, dpi, fn, tau, r, THC):
@@ -114,15 +140,13 @@ def po42(time, C_0, V, F_r, dpi, fn, tau, r, THC):
     C_s = result.y[0]  # surface box concentration in mol/m^3
     C_d = result.y[1]  # deep box concentration in mol/m^3
 
-    print(
-        f"tau = C_s = {C_s[-1]:.2e} mol/m^2, C_d = {C_d[-1]:.2e} mol/m^3"
-    )
+    print(f"tau = C_s = {C_s[-1]:.2e} mol/m^2, C_d = {C_d[-1]:.2e} mol/m^3")
     fig, ax = plt.subplots()
     ax.plot(t, C_s, color="C0", label="Surface Ocean")
     if show_deep_ocean_data:
         ax.plot(t, C_d, color="C1", label="Deep Ocean")
         ax.set_xlabel("Time [Myr]")
-        ax.set_ylabel("P concentration [mol/m^3]")
+        ax.set_ylabel("P concentration [\mu mol/l]")
         ax.legend()
         ax.grid()
         fig.tight_layout()
@@ -130,3 +154,19 @@ def po42(time, C_0, V, F_r, dpi, fn, tau, r, THC):
         fig.set_dpi(dpi)
         fig.savefig(fn)
 
+
+if __name__ == "__main__":
+    import po4
+
+    F_r = 1e3  # River (weathering) flux of PO4 mol/s
+    V = 1.33e18  # Volume of ocean [m^3]
+    C_0 = [6]  # initial P concentration in ocean umol/l
+    time = 4e6  # model run time in years
+    plot_dpi = 300  #
+    figure_name = "po41.png"
+
+    # ---------- no user serviceable parts below ---------- #
+    print(f"Weathering Flux = {F_r:.2e} [mol/s])")
+    print(f"Volume = {V:.2e} m^3"),
+    print(f"C_0 = {C_0[0]:.2f} umol/l)")
+    C = po4.po41(time, C_0, V, F_r, plot_dpi, figure_name, plot=False)
